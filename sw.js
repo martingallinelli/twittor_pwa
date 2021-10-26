e4faa01a4705d3ab9b4303b38d2f5f1e73ef799c
Martin G. Gallinelli <70105281+martingallinelli@users.noreply.github.com>
2021-10-26 19:53:26 -0300

    UPDATE modificaciones iphone + caches

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 // imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // archivos necesiarios para el funcionamiento de la aplicación
 const APP_SHELL = [
-    //'/', no funciona en produccion
+    '/', //no funciona en produccion
     'index.html',
     'css/style.css',
     'js/app.js',
@@ -49,6 +49,11 @@ self.addEventListener('activate', e => {
             if ( key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete( key );
             }
+
+            // si el cache dynamic no es el actual, lo eliminamos
+            if ( key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete( key );
+            }
         });
     });
 
@@ -65,7 +70,7 @@ self.addEventListener('fetch', e => {
             if ( res ) {
                 return res;
             } else {
-                //console.log('No existe en caché, ' + e.request.url);
+                console.log('No existe en caché, ' + e.request.url);
 
                 // si no existe en caché, lo buscamos en el servidor
                 return fetch( e.request )
